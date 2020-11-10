9d21072cf92ddc5115ab2704bb05116fae87624d
kaerickson20000 <74196501+kaerickson20000@users.noreply.github.com>
2020-11-10 09:43:33 +0200

    Add files via upload

---
+++ ExcelProject/UploadMultipleImage.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="UploadMultipleImage.aspx.cs" Inherits="ExcelProject.UploadMultipleImage" %>
+<html >
+<head runat="server">
+    <title>Multiple file Upload</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />  
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" />  
+    <%--
+    <script src="http://jquery-multifile-plugin.googlecode.com/svn/trunk/jquery.js" type="text/javascript"></script>
+    <script src="http://jquery-multifile-plugin.googlecode.com/svn/trunk/jquery.MultiFile.js" type="text/javascript"></script>
+   --%>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:FileUpload ID="FileUploadJquery" runat="server" class="multi" accept="jpg|png" AllowMultiple="true" />
+            <asp:Button runat="server" Text ="Upload File" OnClick ="btnUpload_Click" Class ="btn btn-sm btn-info" />
+
+            <asp:Label ID="lblMessage" runat="server"></asp:Label>  
+        </div>
+    </form>
+</body>
+</html>
---
+++ ExcelProject/UploadMultipleImage.aspx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+// Add this for Input & Output
+using System.IO;
+
+namespace ExcelProject
+{
+    public partial class UploadMultipleImage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // On the page load ----- > At Start up ....
+        }
+        protected void btnUpload_Click(object sender, EventArgs e)
+        {
+            // Upload single image here ....
+            // ----------------------------------------------------------------------------
+
+            //string filename = Path.GetFileName(FileUploadJquery.PostedFile.FileName);
+            //if (filename != "")
+            //{
+            //    FileUploadJquery.SaveAs(Server.MapPath("~/Multiple_Photo/" + filename));
+            //    lblMessage.Text = "Your file uploaded successfully ...";
+            //    lblMessage.ForeColor = System.Drawing.Color.Green;
+            //}
+            //else {
+            //    lblMessage.Text = "Your file not uploaded";
+            //    lblMessage.ForeColor = System.Drawing.Color.Red;
+            //}
+
+            if (FileUploadJquery.HasFile == false)
+            {
+                ScriptManager.RegisterStartupScript(Page, Page.GetType(), "key", "<script>alert('No File Uploaded.')</script>", false);
+            }
+            else
+            {
+                foreach (var file in FileUploadJquery.PostedFiles)
+                {
+                    // string filename = Path.GetFileName(fileuploadimages.PostedFile.FileName);
+                    string filename = Path.GetFileName(file.FileName);
+                    // fileuploadimages.SaveAs(Server.MapPath("~/Multiple_Photo/" + filename));
+                    file.SaveAs(Server.MapPath("./Multiple_Photo/" + filename));
+                }
+                lblMessage.Text = "Your file uploaded successfully ...";
+                lblMessage.ForeColor = System.Drawing.Color.Green;
+            }
+
+        }
+    }
+}
